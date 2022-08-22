b3c7192f08af0bacb059b4364367c6704dfc1831
Abigail <neneojingwa@gmail.com>
2022-08-22 15:57:26 +0100

    first commit

---
+++ more-func.js
@@ -0,0 +1,64 @@
+// passing functions as an array
+const add = function(x,y){
+    return  x + y
+}
+const subtract = function(x,y){
+    return  x - y
+}
+const mutiply = function(x,y){
+    return  x * y
+}
+const divide = function(x,y){
+    return  x / y
+}
+const basicOpreator = [add,subtract,mutiply,divide]
+for (let opreator of basicOpreator) {
+   let answer = opreator(50,2)
+   console.log(answer)
+    
+}
+// as an object 
+let opreators = {
+    add : add,
+    mutiply: mutiply,
+    divide : divide
+}
+for (let func in opreators) {
+    let result = opreators[func]
+    console.log(result(4,5))
+
+}
+function square(num,raiseTo){
+    return num ** raiseTo
+}
+// function as return statement
+function motivationsMusic(){
+    let music = {
+        'Non-stop' : 'kaptin ft Teni',
+        'Rise': 'T Dollar',
+        'Try': 'T Blaze',
+
+    }
+    console.log (music)
+}
+function deepWorship(){
+    let music = {
+        'There was Jesus' : 'Zach Williams ft Dolly Parton',
+        'confidence': 'Snactus Real',
+        'God is so Good' : 'Passion'
+    }
+    console.log (music)
+
+}
+function emotionsLevel(level){
+    level = prompt('whats level is your emotion')
+    level = Number(level)
+    if (level <= 5) {
+        motivationsMusic()
+        
+    }
+    if(level >5){
+        deepWorship()
+    }
+}
+emotionsLevel()
